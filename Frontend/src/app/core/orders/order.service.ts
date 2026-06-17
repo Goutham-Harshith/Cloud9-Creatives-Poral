3edeb65d50e41043865fbd26d7d3854ad8152684
Goutham-Harshith <54266143+Goutham-Harshith@users.noreply.github.com>
2026-06-17 14:47:11 +0530

    Deployment related changes

--- Frontend/src/app/core/orders/order.service.ts
+++ Frontend/src/app/core/orders/order.service.ts
@@ -2,7 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+import { environment } from '../../../environments/environment';
+
+const API_BASE_URL = environment.apiBaseUrl;
 
 interface CreateOrderPayload {
   bag: {
